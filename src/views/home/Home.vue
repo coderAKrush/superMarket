38e0fb1e11b3b72aff0da6f52b248dcacadcc6f8
AKrush <71269530+coderAKrush@users.noreply.github.com>
2022-04-01 19:56:08 +0800

    20220401

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,21 @@
 <template>
-  <h2>购物车</h2>
+  <div id="home">
+    <nav-bar class="home-nav"><div slot="center">鹿鹿Shop</div></nav-bar>
+  </div>
 </template>
 
 <script>
+  import NavBar from 'components/common/navbar/NavBar.vue'
   export default {
-    name: "Cart"
+    components: { NavBar },
+    name: "Home"
   }
 </script>
 
 <style scoped>
+  .home-nav {
+    background-color: var(--color-tint);
+    color: white;
+  }
 
 </style>
