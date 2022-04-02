ed650e92e1261d77e8c5cc2ad5432c08a3934d51
GreatWall <GreatWall@tencent.com>
2022-04-02 18:27:54 +0800

    22-4-2

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -2,18 +2,21 @@
   <div id="home">
     <nav-bar class="home-nav"><div slot="center">鹿鹿Shop</div></nav-bar>
     <home-swiper :banners="banners"/>
+    <recommend-view :recommends="recommends"/>
   </div>
 </template>
 
 <script>
   import NavBar from 'components/common/navbar/NavBar.vue'
   import HomeSwiper from './childComps/HomeSwiper.vue'
+  import RecommendView from './childComps/RecommendView.vue'
   import getPicture from './mocks'
 
   export default {
     components: { 
       NavBar,
-      HomeSwiper 
+      HomeSwiper,
+      RecommendView
     },
     name: "Home",
     data() {
---
+++ src/views/home/childComps/RecommendView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="recommend">
+    <div v-for="(item, index) in recommends" :key="index">
+      <img :src="item.img" alt="">
+      <div>{{item.title}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RecommendView",
+  props: {
+    recommends: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  }
+}
+</script>
